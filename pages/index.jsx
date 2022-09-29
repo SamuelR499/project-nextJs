6521dc2d28c14dd27faf5382f74d0a4ae4da5883
Samuelr499 <samuelribeiro2@hotmail.com>
2022-09-28 23:10:01 -0300

    Style: adiciona css através dos recursos do Next

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,25 @@
+import Link from '../src/components/Link';
+import React from 'react';
+
+function Title({ children, as }) {
+  const Tag = as;
+  return (
+    <React.Fragment>
+      <Tag>
+        {children}
+      </Tag>
+      <style jsx>
+        {`${Tag} {color: red;}`}
+      </style>
+    </React.Fragment>
+  );
+}
+
 export default function HomePage() {
-    return (
-        <div>
-            <h1>Alura Cases - Home</h1>
-            <a href="/faq">Ir para o FAQ</a>
-        </div>
-    )
+  return (
+    <div>
+      <Title as="h1">Alura Cases - Home</Title>
+      <Link href="/faq">Ir para a página do FAQ</Link>
+    </div>
+  );
 }
